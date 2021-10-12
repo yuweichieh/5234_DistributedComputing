47adff9f48b97dd56c1e053bec721bc1a4342ac8
BroSheng <30943636+BroSheng@users.noreply.github.com>
2021-10-12 16:29:30 -0400

    Redo Project Configuration

--- MyShoes/src/main/java/edu/osu/cse5234/util/ServiceFacade.java
+++ MyShoes/src/main/java/edu/osu/cse5234/util/ServiceFacade.java
@@ -3,7 +3,6 @@ import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 
-import javax.json.Json;
 import javax.json.JsonObject;
 import javax.json.JsonValue;
 import javax.json.bind.Jsonb;
