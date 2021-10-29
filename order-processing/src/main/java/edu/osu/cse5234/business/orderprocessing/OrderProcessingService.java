4ae923687fddc10bfb1e84965db153e82908eb48
BroSheng <30943636+BroSheng@users.noreply.github.com>
2021-10-29 12:13:41 -0400

    Updated

--- order-processing/src/main/java/edu/osu/cse5234/business/orderprocessing/OrderProcessingService.java
+++ order-processing/src/main/java/edu/osu/cse5234/business/orderprocessing/OrderProcessingService.java
@@ -1,8 +1,12 @@
 package edu.osu.cse5234.business.orderprocessing;
 
+import javax.json.Json;
+import javax.json.JsonObject;
 import javax.json.bind.*;
 import javax.persistence.*;
 import javax.ws.rs.*;
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.core.*;
 
 @Path("order")
@@ -20,7 +24,7 @@ public class OrderProcessingService {
 	    EntityManagerFactory emfactory = Persistence.createEntityManagerFactory("order-processing");
 	    EntityManager entityManager = emfactory.createEntityManager();
 		    
-       entityManager.getTransaction().begin();
+        entityManager.getTransaction().begin();
 		entityManager.persist(submittedOrder);
 		entityManager.getTransaction().commit();
 		entityManager.close();
@@ -28,6 +32,26 @@ public class OrderProcessingService {
         // nothing to do with submitted order for now
         // just return a hard coded confirmation code
         System.out.println("Order received from customer: \n" + submittedOrder);
+        
+        // build a JsonObject for credit card payment processing
+        JsonObject jsonObj = Json.createObjectBuilder()
+               .add("entityName", "MyGolf")
+               .add("entityAccountNumber", "123456789")
+               .add("creditCardNumber", submittedOrder.getPaymentInfo().getCardNumber())
+               .add("cardHolderName", submittedOrder.getPaymentInfo().getCardHolderName())
+               .add("expirationDate", submittedOrder.getPaymentInfo().getExpirationDate())
+               .add("cvv", submittedOrder.getPaymentInfo().getCvv())
+               .build();
+        
+        // process payment and get confirmation
+        System.out.println("Calling bank to process payment ...");
+        Client client = ClientBuilder.newClient();
+        String paymentConfirm = client.target(PMT_SERVICE_URI)
+        .request(MediaType.APPLICATION_JSON)
+        	.post(Entity.json(jsonObj), String.class);
+        client.close();
+        System.out.println("Payment processing complete, confirmation# is: " + paymentConfirm);
+
         
         return Response.ok("1001").build();
 	}
---
+++ payment-processing/src/main/java/edu/osu/cse5234/business/paymentprocessing/CreditCardProcessingService.java
@@ -0,0 +1,9 @@
+package edu.osu.cse5234.business.paymentprocessing;
+
+import javax.ws.rs.*;
+
+
+@Path("credit-card-processing")
+public class CreditCardProcessingService {
+
+}
